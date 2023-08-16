60ee417e839b601b052d1468665b282c268c58cf
camer <128632619+judicael10@users.noreply.github.com>
2023-08-16 21:21:32 +0100

    Add files via upload

---
+++ appli.js
@@ -0,0 +1,8 @@
+function show(){
+    document.querySelector('.iconk').classList.toggle('open')
+    document.querySelector('.navigation').classList.toggle('avctive')
+
+    document.querySelector('.oliver').classList.toggle('bres')
+
+    document.querySelector('header').classList.toggle('chen')
+}
